1a8a80a01be813cd7767b1984a8a8f7b5420827d
jcasben <jcasben02@gmail.com>
2024-04-23 14:11:30 +0200

    Fix: corrected form and developed login logic

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,16 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import {environment} from "../../enviroments/environment";
+import {LoginRequest} from "./LoginRequest";
+import {FormBuilder, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   standalone: true,
+  imports: [
+    ReactiveFormsModule
+  ],
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  constructor(private http: HttpClient) { }
 
-  login(username: string, password: string) {
-    // Implementar la lógica de inicio de sesión aquí
+  fb = inject(FormBuilder);
+  http = inject(HttpClient);
+
+  form = this.fb.nonNullable.group({
+    username: ['', Validators.required],
+    password: ['', Validators.required]
+  });
+
+  private serverUrl = environment.apiBaseUrl;
+
+  onSubmit(): void {
+    console.log(this.form.getRawValue())
+    this.http.post<{ user: LoginRequest }>(
+      `${this.serverUrl}/auth/login`,
+      this.form.getRawValue(),
+    ).subscribe(response => {
+      console.log('response', response);
+    })
   }
+
 }
