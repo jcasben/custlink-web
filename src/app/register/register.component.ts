ca58450fac7ea4af7574fefb0d99519740321b95
jcasben <jcasben02@gmail.com>
2024-04-23 14:11:50 +0200

    Fix: corrected form and developed register logic

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,17 +1,38 @@
-import { Component } from '@angular/core';
-import {FormsModule} from "@angular/forms";
+import {Component, inject} from '@angular/core';
+import {FormBuilder, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../enviroments/environment";
+import {RegisterRequest} from "./RegisterRequest";
 
 @Component({
   selector: 'app-register',
   standalone: true,
   imports: [
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-  register(username: string, email: string, password: string) {
-    // Implementar la lógica de registro aquí
+
+  private serverUrl = environment.apiBaseUrl;
+  fb = inject(FormBuilder);
+  http = inject(HttpClient);
+
+  form = this.fb.nonNullable.group({
+    username: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', Validators.required]
+  });
+
+  onSubmit(): void {
+    console.log(this.form.getRawValue());
+    this.http.post<{ user: RegisterRequest }>(
+      `${this.serverUrl}/auth/register`,
+      this.form.getRawValue(),
+    ).subscribe(response => {
+      console.log('response', response);
+    });
   }
 }
