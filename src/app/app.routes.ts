21d01524f371d4b3ffaa5489f81ff58181cc6958
jcasben <jcasben02@gmail.com>
2024-04-24 11:08:52 +0200

    Update: added new route to dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import {LoginComponent} from "./login/login.component";
 import {LandingComponent} from "./landing/landing.component";
 import {RegisterComponent} from "./register/register.component";
+import {DashboardComponent} from "./dashboard/dashboard.component";
 
 export const routes: Routes = [
   { path: '', component: LandingComponent, pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent }
+  { path: 'register', component: RegisterComponent },
+  { path: 'dashboard', component: DashboardComponent }
 ];
