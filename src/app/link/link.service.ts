1b386482f026a2cb57b40d6df0af1a35cb289d04
jcasben <jcasben02@gmail.com>
2024-04-25 00:32:07 +0200

    Update: added crud operations to link service

--- src/app/link/link.service.ts
+++ src/app/link/link.service.ts
@@ -1,6 +1,8 @@
-import {Injectable} from "@angular/core";
+import {inject, Injectable} from "@angular/core";
 import {environment} from "../../enviroments/environment";
 import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Link} from "./Link";
 
 @Injectable({
   providedIn: 'root'
@@ -8,9 +10,21 @@ import {HttpClient} from "@angular/common/http";
 export class LinkService {
 
   private apiServerUrl = environment.apiBaseUrl;
-  constructor(private http: HttpClient) {}
+  http = inject(HttpClient);
 
-  public getLinks() {
-    return this.http.get(`${this.apiServerUrl}/link`);
+  public getLinks(): Observable<Link[]> {
+    return this.http.get<Link[]>(`${this.apiServerUrl}/link/links`);
+  }
+
+  public createLink(link: Link): Observable<Link> {
+    return this.http.post<Link>(`${this.apiServerUrl}/link/createLink`, link);
+  }
+
+  public updateLink(name: string, link: Link): Observable<Link> {
+    return this.http.put<Link>(`${this.apiServerUrl}/link/update/${name}`, link);
+  }
+
+  public deleteLink(name: string): Observable<void> {
+    return this.http.delete<void>(`${this.apiServerUrl}/link/delete/${name}`);
   }
 }
