818a3047e1a794b7305cafee95797d488a7b8779
jcasben <jcasben02@gmail.com>
2024-04-25 00:34:44 +0200

    Update: implemented auth check on init (bugged)

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,6 +1,9 @@
-import {Component, inject} from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {RouterLink, RouterLinkActive, RouterOutlet} from "@angular/router";
 import {AuthService} from "../auth/auth.service";
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../enviroments/environment";
+import {User} from "../user/User";
 
 @Component({
   selector: 'app-landing',
@@ -13,6 +16,27 @@ import {AuthService} from "../auth/auth.service";
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
 })
-export class LandingComponent {
+export class LandingComponent implements OnInit {
+
+  serverUrl = environment.apiBaseUrl;
   authService = inject(AuthService);
+  http = inject(HttpClient);
+
+  ngOnInit(): void {
+    this.http.get<{ user: User }>(`${this.serverUrl}/users/user`)
+      .subscribe({
+        next: (response) => {
+          console.log('response', response.user);
+          this.authService.currentUserSig.set(response.user);
+        },
+        error: () => {
+          this.authService.currentUserSig.set(null);
+        },
+      });
+  }
+
+  logout(): void {
+    localStorage.setItem('token', '');
+    this.authService.currentUserSig.set(null);
+  }
 }
