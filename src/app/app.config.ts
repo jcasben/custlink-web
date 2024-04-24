f31c3c496761a159a349eda3fdd71204b9c2c700
jcasben <jcasben02@gmail.com>
2024-04-24 18:37:59 +0200

    Update: added auth interceptor in http client

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,8 +3,9 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import {provideHttpClient, withFetch} from "@angular/common/http";
+import {provideHttpClient, withFetch, withInterceptors} from "@angular/common/http";
+import {authInterceptor} from "./auth/auth.interceptor";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideHttpClient(withFetch())]
+  providers: [provideRouter(routes), provideClientHydration(), provideHttpClient(withFetch(), withInterceptors([authInterceptor]))]
 };
