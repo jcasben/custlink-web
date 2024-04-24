270e05e76eefc318a479a479bd5b4fb2e31aab1a
jcasben <jcasben02@gmail.com>
2024-04-25 00:39:04 +0200

    Update: developed logout function

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,13 +1,26 @@
-import {Component, inject} from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {AuthService} from "../auth/auth.service";
+import {Router, RouterLink} from "@angular/router";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+
   authService = inject(AuthService);
+  http = inject(HttpClient);
+  router = inject(Router);
+
+  logout(): void {
+    localStorage.setItem('token', '');
+    this.authService.currentUserSig.set(null);
+    this.router.navigateByUrl('/login').then();
+  }
 }
