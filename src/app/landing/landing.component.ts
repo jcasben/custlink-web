86c0e993e1a66a8caf9d005368cc8f16dc1ce8b0
jcasben <jcasben02@gmail.com>
2024-04-25 15:44:45 +0200

    Update: deleted unused elements

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,9 +1,5 @@
-import {Component, inject, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {RouterLink, RouterLinkActive, RouterOutlet} from "@angular/router";
-import {AuthService} from "../auth/auth.service";
-import {HttpClient} from "@angular/common/http";
-import {environment} from "../../enviroments/environment";
-import {User} from "../user/User";
 
 @Component({
   selector: 'app-landing',
@@ -16,14 +12,4 @@ import {User} from "../user/User";
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
 })
-export class LandingComponent {
-
-  serverUrl = environment.apiBaseUrl;
-  authService = inject(AuthService);
-  http = inject(HttpClient);
-
-  logout(): void {
-    localStorage.setItem('token', '');
-    this.authService.currentUserSig.set(null);
-  }
-}
+export class LandingComponent {}
