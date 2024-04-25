179392e1e41480dad622c7ea30df5a1383adfb5c
jcasben <jcasben02@gmail.com>
2024-04-25 15:40:52 +0200

    Update: deleted hydration provider import

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,6 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
 import {provideHttpClient, withFetch, withInterceptors} from "@angular/common/http";
 import {authInterceptor} from "./auth/auth.interceptor";
 
