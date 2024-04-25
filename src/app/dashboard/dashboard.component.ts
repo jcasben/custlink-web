fc7ea14df3caa6ac744c257e9a09f58491e74a7e
jcasben <jcasben02@gmail.com>
2024-04-25 15:42:13 +0200

    Update: checked auth in dashboard component

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,25 +2,43 @@ import {Component, inject, OnInit} from '@angular/core';
 import {AuthService} from "../auth/auth.service";
 import {Router, RouterLink} from "@angular/router";
 import {HttpClient} from "@angular/common/http";
+import {LinkComponent} from "../link/link.component";
+import {User} from "../user/User";
+import {environment} from "../../enviroments/environment";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   imports: [
     RouterLink,
+    LinkComponent,
   ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
 
   authService = inject(AuthService);
   http = inject(HttpClient);
   router = inject(Router);
+  serverUrl = environment.apiBaseUrl;
 
   logout(): void {
     localStorage.setItem('token', '');
     this.authService.currentUserSig.set(null);
     this.router.navigateByUrl('/login').then();
+  }
+
+  ngOnInit(): void {
+    this.http.get(`${this.serverUrl}/users/user`)
+      .subscribe({
+        next: (response) => {
+          console.log('response', response as User);
+          this.authService.currentUserSig.set(response as User);
+        },
+        error: () => {
+          this.authService.currentUserSig.set(null);
+        },
+      });
   }
 }
