d742d55b182534c14cdd165f2aff5c46e05d58cc
jcasben <jcasben02@gmail.com>
2024-04-27 19:39:14 +0200

    Update: deleted footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,12 @@ import { Component } from '@angular/core';
 import {RouterLink, RouterOutlet} from '@angular/router';
 import {CommonModule} from "@angular/common";
 import {LandingComponent} from "./landing/landing.component";
-import {FooterComponent} from "./footer/footer.component";
 import {HeaderComponent} from "./header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, LandingComponent, FooterComponent, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, RouterLink, LandingComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
