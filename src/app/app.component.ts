63ff36990c7d98b9e08f96f875ded12d529262fd
jcasben <jcasben02@gmail.com>
2024-04-27 02:19:23 +0200

    Update: added footer to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import {RouterLink, RouterOutlet} from '@angular/router';
 import {CommonModule} from "@angular/common";
 import {LandingComponent} from "./landing/landing.component";
+import {FooterComponent} from "./footer/footer.component";
+import {HeaderComponent} from "./header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, LandingComponent],
+  imports: [CommonModule, RouterOutlet, RouterLink, LandingComponent, FooterComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
