07ca94f5eee0ac62b54ac78a02e7b7f7f5559996
jcasben <jcasben02@gmail.com>
2024-04-27 02:15:32 +0200

    Feat: imported header

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,14 +1,16 @@
 import {Component} from '@angular/core';
 import {RouterLink, RouterLinkActive, RouterOutlet} from "@angular/router";
+import {HeaderComponent} from "../header/header.component";
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [
-    RouterLink,
-    RouterOutlet,
-    RouterLinkActive
-  ],
+    imports: [
+        RouterLink,
+        RouterOutlet,
+        RouterLinkActive,
+        HeaderComponent
+    ],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
 })
