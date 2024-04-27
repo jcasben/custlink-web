7f99daf5782664b99665146bc4a72b52e9da170c
jcasben <jcasben02@gmail.com>
2024-04-27 19:39:34 +0200

    Update: updated landing page

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,16 +1,18 @@
 import {Component} from '@angular/core';
 import {RouterLink, RouterLinkActive, RouterOutlet} from "@angular/router";
 import {HeaderComponent} from "../header/header.component";
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-    imports: [
-        RouterLink,
-        RouterOutlet,
-        RouterLinkActive,
-        HeaderComponent
-    ],
+  imports: [
+    RouterLink,
+    RouterOutlet,
+    RouterLinkActive,
+    HeaderComponent,
+    NgOptimizedImage
+  ],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
 })
