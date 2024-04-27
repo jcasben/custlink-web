6dd48e88e951eaa485ecb67fb1afcf938118858a
jcasben <jcasben02@gmail.com>
2024-04-27 02:17:18 +0200

    Update: moved check user login onInit

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,44 +1,29 @@
 import {Component, inject, OnInit} from '@angular/core';
 import {AuthService} from "../auth/auth.service";
-import {Router, RouterLink} from "@angular/router";
+import {Router, RouterLink, RouterOutlet} from "@angular/router";
 import {HttpClient} from "@angular/common/http";
 import {LinkComponent} from "../link/link.component";
 import {User} from "../user/User";
 import {environment} from "../../enviroments/environment";
+import {NgOptimizedImage} from "@angular/common";
+import {HeaderComponent} from "../header/header.component";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [
-    RouterLink,
-    LinkComponent,
-  ],
+    imports: [
+        RouterLink,
+        LinkComponent,
+        NgOptimizedImage,
+        HeaderComponent,
+        RouterOutlet,
+    ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent {
 
   authService = inject(AuthService);
   http = inject(HttpClient);
   router = inject(Router);
-  serverUrl = environment.apiBaseUrl;
-
-  logout(): void {
-    localStorage.setItem('token', '');
-    this.authService.currentUserSig.set(null);
-    this.router.navigateByUrl('/login').then();
-  }
-
-  ngOnInit(): void {
-    this.http.get(`${this.serverUrl}/users/user`)
-      .subscribe({
-        next: (response) => {
-          console.log('response', response as User);
-          this.authService.currentUserSig.set(response as User);
-        },
-        error: () => {
-          this.authService.currentUserSig.set(null);
-        },
-      });
-  }
 }
